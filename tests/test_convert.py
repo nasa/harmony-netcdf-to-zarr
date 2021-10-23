8b84aedb5d50189f40dfb127733e533e980c067e
Hailiang Zhang <hailiang.zhang@nasa.gov>
2021-10-23 00:09:31 -0400

    HARMONY-953: convert test added

---
+++ tests/test_convert.py
@@ -0,0 +1,28 @@
+"""
+Tests the Harmony convert module
+"""
+
+import unittest
+import pytest
+
+from harmony_netcdf_to_zarr import convert
+
+
+class TestConvert(unittest.TestCase):
+    """
+    Tests the Harmony adapter
+    """
+    def setUp(self):
+        pass
+
+    def test_suggest_chunksize(self):
+        """
+        Test for suggest_chunksize method
+        """
+        #pytest.set_trace()
+        chunksize_expected = (1000,1000)
+        chunksize_result = convert.suggest_chunksize(shape=(1000,1000), datatype='f8')
+        assert chunksize_expected == chunksize_result
+
+    def tearDown(self):
+        pass
